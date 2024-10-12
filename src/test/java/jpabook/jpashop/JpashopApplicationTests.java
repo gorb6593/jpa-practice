5e3a6734369d9647d62e1a61a10fc41e5f8d4c51
haekyu <gorb6593@gmail.com>
2024-10-12 13:34:44 +0900

    TIL

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,13 +1,39 @@
 package jpabook.jpashop;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.entity.Hello;
+import jpabook.jpashop.entity.QHello;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
 @SpringBootTest
 class JpashopApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
+
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+//		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+//
+//		Hello result = query
+//				.selectFrom(qHello)
+//				.fetchOne();
+//
+//		assertThat(result).isEqualTo(hello);
+
 	}
 
 }
